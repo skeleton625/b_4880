13f3589a62e3759d535804ed2244e930dc057482
WaniFi <skelpd24@gmail.com>
2018-08-23 21:53:11 +0900

    4880 문제 풀이 개선

--- baekjoon_4880/main.cpp
+++ baekjoon_4880/main.cpp
@@ -1,15 +1,10 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
-int n[3], a, b;
+int a, b, c;
 int main() {
-	while (1) {
-		for (int i = 0; i < 3; i++) cin>>n[i];
-		if (!n[0] && !n[1] && !n[2]) break;
-		a = n[2] - n[1];
-		b = n[1] - n[0];
-		if (a == b) cout << "AP " << b + n[2] << endl;
-		else cout << "GP " << n[2] + a*a/b << endl;
+	while (scanf_s("%d %d %d",&a,&b,&c) && (a||b||c)) {
+		if (b*2==a+c) printf("AP %d\n", (b-a)+c);
+		else printf("GP %d\n", c*b/a);
 	}
 	return 0;
 }
